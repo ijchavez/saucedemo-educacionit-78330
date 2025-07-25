66012a1b1d98b5e3555fad074a14d26194e8a05f
Gerardo Chavez <info.gerardo.chavez@gmail.com>
2025-07-25 13:39:35 -0300

    check github run

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/resources/features",
-    glue = {"src/test/java/stepsdefinitions"},
+    features = "src/test/resources",
+	glue = {"stepdefinitions"},
     plugin = {"pretty", "html:target/cucumber-report.html"},
     monochrome = true
 )
